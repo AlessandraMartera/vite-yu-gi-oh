4a6ebadf642904e310a841bd8f107fdb241c5109
alessandra martera <ale.martera@gmail.com>
2023-06-05 15:58:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import ListCard from './components/ListCard.vue'
+export default {
+  components: {
+    AppHeader,
+    ListCard
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <main>
+    <ListCard />
+  </main>
+</template>
+
+<style lang="scss">
+@use './styles/generals.scss' as *;
+@use './styles/partials/variables.scss' as *;
+
+body {
+  background-color: #d48f38;
+}
+</style>
