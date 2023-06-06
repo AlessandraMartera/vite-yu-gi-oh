da81f0a3e06f66eb0b1d2669336f658d6bc17cd1
alessandra martera <ale.martera@gmail.com>
2023-06-06 17:01:24 +0200

    update start the $emit form @change

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -16,20 +16,22 @@ export default {
 <template>
     <section>
         <form>
-            <select name="archetype" id="searchType" v-model.trim="store.searchType">
-                <option v-for="archetype in store.arrayOfType" :value="archetype.archetype_name">{{ archetype.archetype_name
-                }}</option>
-
+            <select name="archetype" id="searchType" v-model.trim="store.searchType" @change="$emit('searchType')">
+                <option v-for=" archetype in store.arrayOfType" :value="archetype.archetype_name">
+                    {{ archetype.archetype_name }}
+                </option>
             </select>
-
-            <button type="submit" @click.prevent="$emit('searchType')">
-                cerca
-            </button>
+            <!-- 
+                            <button type="submit" @click.prevent="$emit('searchType')">
+                                cerca
+                            </button> -->
         </form>
     </section>
 </template>
 
 <style lang="scss" scoped>
+@use './../styles/partials/variables.scss' as *;
+
 #searchType {
     margin: 8px;
     width: 100px;
@@ -37,5 +39,14 @@ export default {
     background-color: #fff;
     border: 1px solid #d4d3cf;
     border-radius: 5px;
+}
+
+button {
+    cursor: pointer;
+    padding: 10px;
+    border: 1px solid #fff;
+    border-radius: 10px;
+    color: $primaryColor;
+    background-color: #fff;
 }
 </style>
